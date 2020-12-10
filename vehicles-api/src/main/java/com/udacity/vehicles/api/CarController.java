fa23419c7b1083325873d0f34a76e6d0c8ed8373
qmehdi <qmehdi110@gmail.com>
2020-12-10 17:09:02 -0600

    tmp - dont know how to post a car object

--- vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -64,9 +64,7 @@ class CarController {
          *   Update the first line as part of the above implementing.
          */
         // Find by Id method
-        carService.findById(id);
-
-        return assembler.toResource(new Car());
+        return assembler.toResource(carService.findById(id));
     }
 
     /**
@@ -82,7 +80,7 @@ class CarController {
          * TODO: Use the `assembler` on that saved car and return as part of the response.
          *   Update the first line as part of the above implementing.
          */
-        Resource<Car> resource = assembler.toResource(new Car());
+        Resource<Car> resource = assembler.toResource(carService.save(car));
         return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
