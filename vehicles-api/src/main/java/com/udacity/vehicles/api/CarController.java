b4c51ebff59c46deaaabe9212f78a6c6b5dbef40
qmehdi <qmehdi110@gmail.com>
2020-12-11 14:42:02 -0600

    Able to update an existing car using PUT

--- vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -91,15 +91,21 @@ class CarController {
      * @return response that the vehicle was updated in the system
      */
     @PutMapping("/{id}")
-    ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) {
+    ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) throws URISyntaxException {
         /**
          * TODO: Set the id of the input car object to the `id` input.
          * TODO: Save the car using the `save` method from the Car service
          * TODO: Use the `assembler` on that updated car and return as part of the response.
          *   Update the first line as part of the above implementing.
          */
-        Resource<Car> resource = assembler.toResource(new Car());
-        return ResponseEntity.ok(resource);
+        // Set Id of the input car object to the `id` input
+        car.setId(id);
+
+        // Save the car using the `save` method from the Car service
+        carService.save(car);
+
+        Resource<Car> resource = assembler.toResource(carService.save(car));
+        return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
     /**
