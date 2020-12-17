ef74d37b6bdbbb30894a2c054236ad4be9b63d57
qmehdi <qmehdi110@gmail.com>
2020-12-17 17:04:38 -0600

    Added swagger-ui /swagger-ui.html

--- vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.reactive.function.client.WebClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Launches a Spring Boot application for the Vehicles API,
@@ -18,6 +19,7 @@ import org.springframework.web.reactive.function.client.WebClient;
  */
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class VehiclesApiApplication {
 
     public static void main(String[] args) {
