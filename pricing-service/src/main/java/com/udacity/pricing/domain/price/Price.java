536bd6281068ef008e027f2b173d67c3c1cd1cd6
qmehdi <qmehdi110@gmail.com>
2020-12-17 17:42:22 -0600

    Converted pricing into a microservice. Able to fetch price by passing in a vehicleId

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,11 +1,20 @@
 package com.udacity.pricing.domain.price;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String currency;
     private BigDecimal price;
@@ -18,6 +27,14 @@ public class Price {
         this.currency = currency;
         this.price = price;
         this.vehicleId = vehicleId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getCurrency() {
--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,8 +1,25 @@
 package com.udacity.pricing.domain.price;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class PriceRepository {
+import java.util.List;
 
+@Repository
+public interface PriceRepository extends CrudRepository<Price, Long> {
+
+    /** This allows us to call this method via POST as follows
+        http://localhost:8082/prices/search/retrieveByVehicleId?vehicleId=100
+            But first, POST a price as follows:
+                http://localhost:8082/prices
+                 {
+                     "currency": "USD",
+                     "price": 22122.99,
+                     "vehicleId": 100
+                 }
+     */
+    @RestResource(path = "retrieveByVehicleId", rel = "customFindMethod")
+    List<Price> findByVehicleId(@Param("vehicleId") Long vehicleId);
 }
