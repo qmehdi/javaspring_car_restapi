b36d7f366206cdae79c350fa036fd9861c052d96
qmehdi <qmehdi110@gmail.com>
2020-12-25 21:35:03 -0600

    Pricing-service: Added integration test for get price for specific vehicleId

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,46 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PricingServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+	private HttpHeaders headers = new HttpHeaders();
+
+	private String createURLWithPort(String uri) {
+		return "http://localhost:" + port + uri;
 	}
 
+	@Test
+	public void getPriceForSpecificVehicle() {
+		ResponseEntity<String> response = restTemplate.getForEntity(
+				createURLWithPort("/services/price?vehicleId=1"), String.class);
+
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+		assertNotNull(response.getBody());
+	}
 }
